94d9f50faf6ba102629137ccb9be0209e35d0037
Paloma Piot <paloma.piot@udc.es>
2021-04-14 19:21:27 +0200

    insert image

--- packages/image_repository/lib/src/firebase_image_repository.dart
+++ packages/image_repository/lib/src/firebase_image_repository.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 
+import 'package:path/path.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 import '../image_repository.dart';
 import 'entity/entities.dart';
@@ -45,10 +48,27 @@ class FirebaseImageRepository implements ImageRepository {
   }
 
   @override
-  Future insert(Image image, File file) {
-    print(image);
-    print(file);
-    throw UnimplementedError();
+  Future insert(Image image, File file) async {
+    String fileName = getRandomString(15) + basename(file.path);
+    StorageReference firebaseStorageRef =
+      FirebaseStorage.instance.ref().child('/$fileName');
+    StorageUploadTask uploadTask = firebaseStorageRef.putFile(file);
+    StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
+    taskSnapshot.ref.getDownloadURL().then(
+        (value) => {
+          print("Done: $value"),
+          imagesCollection
+            .add({
+              'author': image.author,
+              'classroom': image.classroom,
+              'date': image.date,
+              'url': value,
+              'validated': image.validated,
+            })
+        } ,
+    );
+
+
   }
 
   @override
@@ -61,3 +81,9 @@ class FirebaseImageRepository implements ImageRepository {
     throw UnimplementedError();
   }
 }
+
+const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+Random _rnd = Random();
+
+String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
+    length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
