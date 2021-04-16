ee0f9462cc31d6cb0016d85d0bc1763a61c27adc
Paloma Piot <paloma.piot@udc.es>
2021-04-16 23:01:45 +0200

    Project page on init

--- lib/images/view/images_page.dart
+++ lib/images/view/images_page.dart
@@ -7,7 +7,6 @@ import 'package:xela_arias/common/models/EntityType.dart';
 import 'package:xela_arias/common/pick_image.dart';
 import 'package:xela_arias/common/widgets/base_page.dart';
 import 'package:xela_arias/common/widgets/infinite_page.dart';
-import 'package:xela_arias/images/bloc/file_card.dart';
 import 'package:xela_arias/navigation/model/app_tab.dart';
 import 'package:image_repository/image_repository.dart';
 
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,10 @@
 class XelaAriasRoutes {
-  static final home = '/';
+  static final home = '/galery';
   static final classroom = '/classroom';
   static final poems = '/poems';
   static final images = '/images';
   static final saved = '/saved';
+  static final project = '/';
 
   static final pickPoem = '/poems/pick';
   static final pickImage = '/images/pick';
